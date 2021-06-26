cb171594e8ffbf17dc7e99dab0c37d0e88bcbf9e
重新读取 <qzadsl87855203@126.com>
2021-06-26 09:48:12 +0800

    no commit message

---
+++ ResumeML/ResumeML/Business/TestBusiness.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResumeML.Business
+{
+    public class TestBusiness
+    {
+        public virtual string printTest()
+        {
+            return "你好啊";
+        }
+    }
+}
