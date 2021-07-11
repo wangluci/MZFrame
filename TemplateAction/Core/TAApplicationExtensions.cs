29692b52363d2ad5f507f54d0b05e0aa6b6fec9a
重新读取 <qzadsl87855203@126.com>
2021-07-11 21:07:59 +0800

    no commit message

---
+++ TemplateAction/Core/TAApplicationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TemplateAction.Core
+{
+    public static class TAApplicationExtensions
+    {
+        public static TAApplication UsePluginPath(this TAApplication app, string path)
+        {
+            app.PluginPath = path;
+            return app;
+        }
+        public static TAApplication Init(this TAApplication app, string rootpath)
+        {
+            app.TAInit(rootpath);
+            return app;
+        }
+    }
+}
