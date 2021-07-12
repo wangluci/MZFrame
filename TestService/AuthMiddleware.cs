563da0df1b19f0051ec7839003b835be98e0826e
重新读取 <qzadsl87855203@126.com>
2021-07-12 21:11:45 +0800

    no commit message

---
+++ TestService/AuthMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TemplateAction.Core;
+
+namespace TestService
+{
+    public class AuthMiddleware : IFilterMiddleware
+    {
+        public object Excute(TARequestHandle request, FilterMiddlewareNode next)
+        {
+            if (request.NameSpace == "testservice" && request.Controller == "home" && request.Action == "test")
+            {
+                //需要身份认证
+                request.Context.Response.Write("带有身份认证\n");
+                return next.Excute(request);
+            }
+            else
+            {
+                //不需要认证
+                return next.Excute(request);
+            }
+        }
+    }
+}
