fccdce8c34d99ec0dc6ede4d52d9526c5f2a3728
重新读取 <qzadsl87855203@126.com>
2021-08-07 22:05:40 +0800

    no commit message

---
+++ MyAccess/Aop/DAL/IDBFactory.cs
@@ -0,0 +1,11 @@
+﻿using MyAccess.DB;
+using System;
+
+namespace MyAccess.Aop.DAL
+{
+    public interface IDBFactory
+    {
+        string Key { get; }
+        IDbHelp CreateHelp();
+    }
+}
---
+++ MyAccess/Aop/DBStore/DBStoreAsync.cs
@@ -0,0 +1,58 @@
+﻿using MyAccess.Aop.DAL;
+using MyAccess.DB;
+using System;
+using System.Threading;
+
+namespace MyAccess.Aop
+{
+    public class DBStoreAsync : IDBStore
+    {
+        private static AsyncLocal<DBStoreItem> _storeItem = new AsyncLocal<DBStoreItem>();
+        public bool IsTranslation()
+        {
+            DBStoreItem dbsi = _storeItem.Value;
+            if (dbsi != null)
+            {
+                return dbsi.IsTranslation();
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public IDbHelp OpenDB(IDBFactory factory)
+        {
+            DBStoreItem dbsi = _storeItem.Value;
+            if (dbsi == null)
+            {
+                dbsi = new DBStoreItem();
+                _storeItem.Value = dbsi;
+            }
+            return dbsi.OpenDB(factory);
+        }
+        public void BeginTrans(Isolation level)
+        {
+            DBStoreItem dbsi = _storeItem.Value;
+            if (dbsi != null)
+            {
+                dbsi.BeginTrans(level);
+            }
+        }
+        public void Commit()
+        {
+            DBStoreItem dbsi = _storeItem.Value;
+            if (dbsi != null)
+            {
+                dbsi.Commit();
+            }
+        }
+        public void RollBack()
+        {
+            DBStoreItem dbsi = _storeItem.Value;
+            if (dbsi != null)
+            {
+                dbsi.RollBack();
+            }
+        }
+    }
+}
