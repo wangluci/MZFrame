23f5669f3b1a6b6e1767af44a1a822bf3037baf2
重新读取 <qzadsl87855203@126.com>
2021-08-09 15:26:01 +0800

    no commit message

---
+++ TemplateAction/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TemplateAction.Core;
+
+namespace TemplateAction.Extension
+{
+    public static class ServiceCollectionExtension
+    {
+        /// <summary>
+        /// 注册字符串依赖
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="name"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddString(this IServiceCollection collection, string name, string val)
+        {
+            collection.Add(name, new ServiceDescriptor(typeof(string), ServiceLifetime.Other, (object[] arguments) =>
+            {
+                return val;
+            }, StringLifetimeFactory));
+            return collection;
+        }
+        public static object StringLifetimeFactory(PluginCollection collection, ServiceDescriptor sd)
+        {
+            return collection.CreateServiceInstance(sd.ServiceType, sd.Factory);
+        }
+    }
+}
