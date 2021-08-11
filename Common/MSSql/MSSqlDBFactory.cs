0f3be449a1e43bd9010a6fec1fe76443e48d5580
重新读取 <qzadsl87855203@126.com>
2021-08-11 15:11:58 +0800

    no commit message

---
+++ Common/MSSql/MSSqlDBFactory.cs
@@ -0,0 +1,22 @@
+﻿using MyAccess.Aop.DAL;
+using MyAccess.DB;
+namespace Common.MSSql
+{
+    public class MSSqlDBFactory : IDBFactory
+    {
+        private string _connstr;
+        public MSSqlDBFactory(string connstr)
+        {
+            _connstr = connstr;
+        }
+        public string Key
+        {
+            get { return _connstr; }
+        }
+
+        public IDbHelp CreateHelp()
+        {
+            return new SqlDbHelp(_connstr);
+        }
+    }
+}
---
+++ Common/MySql/MySqlDBFactory.cs
@@ -0,0 +1,23 @@
+﻿using MyAccess.Aop.DAL;
+using MyAccess.DB;
+
+namespace Common.MySql
+{
+    public class MySqlDBFactory : IDBFactory
+    {
+        private string _connstr;
+        public MySqlDBFactory(string connstr)
+        {
+            _connstr = connstr;
+        }
+        public string Key
+        {
+            get { return _connstr; }
+        }
+
+        public IDbHelp CreateHelp()
+        {
+            return new MySqlHelp(_connstr);
+        }
+    }
+}
