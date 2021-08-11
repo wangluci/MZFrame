a2b2794671c5f715238f47771d551ed83f8a2c20
重新读取 <qzadsl87855203@126.com>
2021-08-11 15:53:55 +0800

    no commit message

---
+++ MyAccess/Aop/DAL/DBSupportBase.cs
@@ -0,0 +1,12 @@
+﻿using MyAccess.DB;
+using System;
+namespace MyAccess.Aop.DAL
+{
+    public abstract class DBSupportBase
+    {
+        internal abstract IDbHelp DBHelp { get; }
+
+        internal abstract bool IsTranslation { get; }
+        internal abstract void InitHelp();
+    }
+}
