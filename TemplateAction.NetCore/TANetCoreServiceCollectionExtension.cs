12657e0a333059e40e66408b1d1ee7ac819b67a3
重新读取 <qzadsl87855203@126.com>
2021-08-25 20:17:58 +0800

    no commit message

--- TemplateAction.NetCore/TANetCoreServiceCollectionExtension.cs
+++ TemplateAction.NetCore/TANetCoreServiceCollectionExtension.cs
@@ -1,14 +1,45 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
 using TemplateAction.Core;
-
 namespace TemplateAction.NetCore
 {
     public static class TANetCoreServiceCollectionExtension
     {
-        //public static void AddNetSingleton<T1>(this IServiceCollection collection, string name, string val)
-        //{
-        //    Type strType = typeof(string);
-        //    collection.AddSingleton<T1>();
-        //}
+        public static IServiceCollection AddOptions(this IServiceCollection collection)
+        {
+            collection.AddSingleton(typeof(IOptions<>), typeof(OptionsManager<>));
+            collection.AddScope(typeof(IOptionsSnapshot<>), typeof(OptionsManager<>));
+            collection.AddSingleton(typeof(IOptionsMonitor<>), typeof(OptionsMonitor<>));
+            collection.AddTransient(typeof(IOptionsFactory<>), typeof(OptionsFactory<>));
+            collection.AddSingleton(typeof(IOptionsMonitorCache<>), typeof(OptionsCache<>));
+            return collection;
+        }
+        public static IServiceCollection Configure<TOptions>(this IServiceCollection services, string name, Action<TOptions> configureOptions) where TOptions : class
+        {
+            services.AddOptions();
+            services.AddSingleton<IConfigureOptions<TOptions>>((object[] arguments) =>
+            {
+                return new ConfigureNamedOptions<TOptions>(name, configureOptions);
+            });
+            return services;
+        }
+        public static IServiceCollection Configure<TOptions>(this IServiceCollection services, Action<TOptions> configureOptions) where TOptions : class
+        {
+            return services.Configure(string.Empty, configureOptions);
+        }
+        public static IServiceCollection Configure<TOptions>(this IServiceCollection services, string name, IConfiguration config) where TOptions : class
+        {
+            services.AddOptions();
+            services.AddSingleton<IOptionsChangeTokenSource<TOptions>>((object[] arguments) =>
+            {
+                return new ConfigurationChangeTokenSource<TOptions>(name, config);
+            });
+            services.AddSingleton<IConfigureOptions<TOptions>>((object[] arguments) =>
+            {
+                return new NamedConfigureFromConfigurationOptions<TOptions>(name, config);
+            });
+            return services;
+        }
     }
 }
