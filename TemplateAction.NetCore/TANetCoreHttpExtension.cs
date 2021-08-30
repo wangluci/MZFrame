d9c104cd59a72827a58c4e4bf96a81d376a933a8
重新读取 <qzadsl87855203@126.com>
2021-08-30 18:00:38 +0800

    no commit message

--- TemplateAction.NetCore/TANetCoreHttpExtension.cs
+++ TemplateAction.NetCore/TANetCoreHttpExtension.cs
@@ -50,46 +50,8 @@ namespace TemplateAction.NetCore
             await ((TANetCoreHttpFile)file).SaveAsAsync(filename);
         }
 
-        /// <summary>
-        /// 微软内置服务转移到TA的服务中
-        /// </summary>
-        /// <param name="app"></param>
-        /// <param name="appBuilder"></param>
-        /// <returns></returns>
-        public static TASiteApplication CopyServicesFrom(this TASiteApplication app, IApplicationBuilder appBuilder)
-        {
-            //映射服务
-            app.Services.AddSingleton<IServiceProvider, TANetServiceProvider>();
 
-            //复制服务
-            Microsoft.Extensions.DependencyInjection.ServiceCollection tservices = appBuilder.ServerFeatures.Get<Microsoft.Extensions.DependencyInjection.ServiceCollection>();
-            foreach (Microsoft.Extensions.DependencyInjection.ServiceDescriptor micsd in tservices)
-            {
-                ServiceLifetime lifetime = ServiceLifetime.Singleton;
-                switch (micsd.Lifetime)
-                {
-                    case Microsoft.Extensions.DependencyInjection.ServiceLifetime.Scoped:
-                        lifetime = ServiceLifetime.Scope;
-                        break;
-                    case Microsoft.Extensions.DependencyInjection.ServiceLifetime.Transient:
-                        lifetime = ServiceLifetime.Transient;
-                        break;
-                    case Microsoft.Extensions.DependencyInjection.ServiceLifetime.Singleton:
-                        lifetime = ServiceLifetime.Singleton;
-                        break;
-                }
-                ProxyFactory pfactory = null;
-                if (micsd.ImplementationFactory != null)
-                {
-                    pfactory = (object[] constructorArguments) =>
-                    {
-                        return micsd.ImplementationFactory.Invoke(app.ServiceProvider.GetService<IServiceProvider>());
-                    };
-                }
-                app.Services.Add(micsd.ServiceType.FullName, new ServiceDescriptor(micsd.ImplementationType, lifetime, pfactory, micsd.ImplementationInstance));
-            }
-            return app;
-        }
+      
         /// <summary>
         /// 设置使用TA的MVC
         /// </summary>
@@ -102,7 +64,8 @@ namespace TemplateAction.NetCore
             {
                 TAEventDispatcher.Instance.RegisterLoadBefore<TASiteApplication>(app =>
                 {
-                    app.CopyServicesFrom(appBuilder);
+                    Microsoft.Extensions.DependencyInjection.ServiceCollection tservices = appBuilder.ServerFeatures.Get<Microsoft.Extensions.DependencyInjection.ServiceCollection>();
+                    app.CopyServicesFrom(tservices);
 
                     //设置路由
                     string defns = null;
