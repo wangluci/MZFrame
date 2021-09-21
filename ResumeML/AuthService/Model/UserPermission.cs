6c95f60509133badfcf1ad427a2309b2490e7fe2
重新读取 <qzadsl87855203@126.com>
2021-09-21 18:57:45 +0800

    no commit message

---
+++ ResumeML/AuthService/Model/UserPermission.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AuthService
+{
+    public class UserPermission
+    {
+        public UserPermission()
+        {
+            UserId = 0;
+            RightCode = "";
+            RightType = 0;
+        }
+        public long UserId { get; set; }
+        public string RightCode { get; set; }
+        public int RightType { get; set; }
+    }
+}
