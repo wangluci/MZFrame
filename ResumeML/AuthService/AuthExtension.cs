084e0200711681a3eec76d75fddc9c33e287b9fa
重新读取 <qzadsl87855203@126.com>
2021-09-25 22:13:51 +0800

    no commit message

---
+++ ResumeML/AuthService/AuthExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TemplateAction.Core;
+
+namespace AuthService
+{
+    public static class AuthExtension
+    {
+        public static T GetItem<T>(this ITAContext context,string key)
+        {
+            return (T)context.Items[key];
+        }
+        public static string GetTerminal(this ITAContext context)
+        {
+            string agent = context.Request.UserAgent.ToLower();
+            if (agent.Contains("micromessenger"))
+            {
+                return "WeiXin:" + context.Request.ClientIP;
+            }
+            else if (agent.Contains("iphone") || agent.Contains("ipod") || agent.Contains("ipad"))
+            {
+                return "ios:" + context.Request.ClientIP;
+            }
+            else if (agent.Contains("android"))
+            {
+                return "android:" + context.Request.ClientIP;
+            }
+            else
+            {
+                return "pc:" + context.Request.ClientIP;
+            }
+        }
+    }
+}
