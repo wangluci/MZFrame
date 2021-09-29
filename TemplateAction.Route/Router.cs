67bc7790179ee664790999cb550a87ec6a2ef21c
重新读取 <qzadsl87855203@126.com>
2021-09-29 21:23:56 +0800

    no commit message

--- TemplateAction.Route/Router.cs
+++ TemplateAction.Route/Router.cs
@@ -45,15 +45,12 @@ namespace TemplateAction.Route
         public IDictionary<string, IRouteConstraint> Constraints { get; protected set; }
         public IDictionary<string, object> Defaults { get; protected set; }
         private RouteTemplateMatcher _matcher;
-        public Router(string template) : this(template, null)
-        {
-        }
-        public Router(string template, IDictionary<string, object> defaults, IDictionary<string, IRouteConstraint> constraints)
+        public Router(string template, IDictionary<string, object> defaults = null, IDictionary<string, IRouteConstraint> constraints = null)
         {
             ParsedTemplate = RouteTemplateParser.Parse(template);
             Constraints = GetConstraints(ParsedTemplate);
             Defaults = GetDefaults(ParsedTemplate, defaults);
-            if (constraints != null && Constraints != null)
+            if (constraints != null)
             {
                 foreach (KeyValuePair<string, IRouteConstraint> kvp in constraints)
                 {
@@ -61,10 +58,7 @@ namespace TemplateAction.Route
                 }
             }
         }
-        public Router(string template, IDictionary<string, object> defaults) : this(template, defaults, null)
-        {
-        }
-        public Router(string template, object defaults, object constraints)
+        public Router(string template, object defaults, object constraints = null)
         {
             Dictionary<string, object> dic = new Dictionary<string, object>();
             PropertyInfo[] props = defaults.GetType().GetProperties();
@@ -76,16 +70,20 @@ namespace TemplateAction.Route
             Constraints = GetConstraints(ParsedTemplate);
             Defaults = GetDefaults(ParsedTemplate, dic);
 
-            Dictionary<string, IRouteConstraint> constdic = new Dictionary<string, IRouteConstraint>();
-            PropertyInfo[] constprops = defaults.GetType().GetProperties();
-            foreach (PropertyInfo p in constprops)
+            if (constraints != null)
             {
-                constdic.Add(p.Name, (IRouteConstraint)p.GetValue(constraints, null));
+                Dictionary<string, IRouteConstraint> constdic = new Dictionary<string, IRouteConstraint>();
+                PropertyInfo[] constprops = defaults.GetType().GetProperties();
+                foreach (PropertyInfo p in constprops)
+                {
+                    constdic.Add(p.Name, (IRouteConstraint)p.GetValue(constraints, null));
+                }
+                foreach (KeyValuePair<string, IRouteConstraint> kvp in constdic)
+                {
+                    Constraints.Add(kvp.Key, kvp.Value);
+                }
             }
-            foreach (KeyValuePair<string, IRouteConstraint> kvp in constdic)
-            {
-                Constraints.Add(kvp.Key, kvp.Value);
-            }
+        
         }
         /// <summary>
         /// 获取默认值
