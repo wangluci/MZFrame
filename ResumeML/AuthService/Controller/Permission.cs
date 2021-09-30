5914b188c7408d8d17c11fbbad4186b7bfa9ec2e
重新读取 <qzadsl87855203@126.com>
2021-09-30 15:59:38 +0800

    no commit message

---
+++ ResumeML/AuthService/Controller/Permission.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using TemplateAction.Core;
+
+namespace AuthService
+{
+    [Des("权限设置")]
+    public class Permission : TABaseController
+    {
+        /// <summary>
+        /// 获取权限列表
+        /// </summary>
+        /// <returns></returns>
+        public AjaxResult GetPermissions()
+        {
+            List<DescribeInfo> tlist = Context.Application.FindAllDescribe();
+            return Success();
+        }
+        [HttpGet]
+        [Des("查看角色")]
+        public AjaxResult GetRoles()
+        {
+            return Success();
+        }
+        [HttpPost]
+        [Des("新增角色")]
+        public AjaxResult PostRole()
+        {
+            return Success();
+        }
+        [HttpPut]
+        [Des("修改角色")]
+        public AjaxResult PutRole()
+        {
+            return Success();
+        }
+        [HttpDelete]
+        [Des("删除角色")]
+        public AjaxResult DeleteRole()
+        {
+            return Success();
+        }
+    }
+}
